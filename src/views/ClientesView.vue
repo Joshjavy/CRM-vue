da46177f9a894b52b39ae3ce255c273c42fd7c2c
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-03-13 15:50:58 -0600

    189. Mostrando el Nombre de la página actual

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,11 +1,14 @@
 <script setup>
     import RouterLink from '@/components/UI/RouterLink.vue';
+    import Heading from '@/components/UI/Heading.vue'
 </script>
 <template>
     <div>
-        <div>
+        <div class="flex justify-end"
+        >
             <RouterLink to="agregar-cliente">Agregar Cliente</RouterLink>
         </div>
-        <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
+        <Heading> Listados de  Clientes </Heading>
+        
     </div>
 </template>
