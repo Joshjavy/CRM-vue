7d7123fea65ed05ef2f565e3a1f75b9bff3b4c57
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-03-15 18:29:28 -0600

    210. Centralizando las URL's de nuestra API en un Servicio

---
+++ src/services/ClientesService.js
@@ -0,0 +1,9 @@
+import api from "@/lib/axios";
+
+export default{
+    obtenerClientes(){
+        return api.get('/clientes');
+    },
+    
+
+}
--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,6 @@
 <script setup>
     import { onMounted, ref, computed } from 'vue';
-    import axios from '@/lib/axios';
+    import ClientesService from '@/services/ClientesService';
     import RouterLink from '@/components/UI/RouterLink.vue';
     import Heading from '@/components/UI/Heading.vue'
     import Cliente from '@/components/Cliente.vue';
@@ -8,7 +8,7 @@
 const clientes = ref([]);
 
 onMounted(() => {//promesas
-    axios('/clientes')
+    ClientesService.obtenerClientes()
         .then(({ data }) => clientes.value = data)
         .catch(error => console.log('Hubo un error'))
 })
