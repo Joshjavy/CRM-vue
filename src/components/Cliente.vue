452a10b3cfa7ac86b04eb641641052caa38e07d8
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-03-29 18:39:48 -0600

    212. Detectando el Estado del Cliente

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -13,6 +13,10 @@ import Cliente from '@/components/Cliente.vue';
         return props.cliente.nombre + ' ' + props.cliente.apellidos
     })
 
+    const estadoCliente=computed(()=>{
+        return props.cliente.estado;
+    })
+
 </script>
 <template>
     <tr>
@@ -25,7 +29,12 @@ import Cliente from '@/components/Cliente.vue';
             <p class="text-gray-600">{{ cliente.puesto }}</p>
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm">
-
+            <button 
+                class="inline-flex rounded-full px-2 text-xs font-semibold leading-5"
+                :class="[estadoCliente? 'bg-green-100 text-green-800' : ' bg-red-100 text-red-800']"
+            >
+                {{ estadoCliente?'Activo':'Inactivo' }}
+            </button>
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
             <RouterLink 
